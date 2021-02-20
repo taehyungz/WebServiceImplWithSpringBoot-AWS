35f5793f34a12a37402994e71457a70ac19f2cf9
thkthk97@naver.com <thkthk97@naver.com>
2021-02-21 02:38:12 +0900

    posts-save, update

--- src/main/java/com/project/spring_study/web_project/service/posts/PostsService.java
+++ src/main/java/com/project/spring_study/web_project/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.project.spring_study.web_project.service.posts;
 
 import com.project.spring_study.web_project.domain.posts.Posts;
 import com.project.spring_study.web_project.domain.posts.PostsRepository;
+import com.project.spring_study.web_project.web.dto.PostsListResponseDto;
 import com.project.spring_study.web_project.web.dto.PostsResponseDto;
 import com.project.spring_study.web_project.web.dto.PostsSaveRequestDto;
 import com.project.spring_study.web_project.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,10 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
